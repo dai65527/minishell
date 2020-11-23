acf746236050adcf3a8a65e8c31efc28450208ed
dnakano <dnakano@student.42tokyo.jp>
2020-11-23 21:49:17 +0900

    Change: main
    return 1に修正します。

--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,20 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 18:37:15 by dnakano           #+#    #+#             */
-/*   Updated: 2020/11/23 19:04:45 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/11/23 21:48:35 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/*
+** comment
+*/
 
 int		main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	return (0);
+	return (1);
 }
