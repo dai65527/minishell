9c5b9fa62d279b95efd5cc08c1395817c58a6963
dnakano <dnakano@student.42tokyo.jp>
2020-11-25 11:08:47 +0900

    Add: msh_exec_cmd
    msh_exec_cmdを追加しました。
    cmd文字列の内容を各コマンドを実行する関数に振り分ける機能を持たせました。

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 18:38:24 by dnakano           #+#    #+#             */
-/*   Updated: 2020/11/25 09:26:27 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/11/25 10:54:08 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@
 ** t_mshinfo (= struct s_mshinfo)
 ** Structure to store information for execution of minishell.
 ** minishellの実行に必要な情報を格納する構造体。
-** 
+**
 ** member
 **  fd_cmdsrc:		コマンドのソースのファイルディスクリプタを格納する。
 **					コンソールからの入力(STDIN) -> fd_cmdsrc = 0
@@ -37,12 +37,12 @@
 ** 	ret_last_cmd:	最後に実行したコマンドの返り値。（=$?）
 */
 
-typedef struct  s_mshinfo
+typedef struct	s_mshinfo
 {
-	t_list      *envlst;
-	int         fd_cmdsrc;
+	t_list		*envlst;
+	int			fd_cmdsrc;
 	int			ret_last_cmd;
-}               t_mshinfo;
+}				t_mshinfo;
 
 /*
 ** t_keyval (= struct s_keyval)
@@ -50,28 +50,29 @@ typedef struct  s_mshinfo
 ** 環境変数を格納するために使うキーバリュー型構造体。
 */
 
-typedef struct  s_keyval
+typedef struct	s_keyval
 {
-	char        *key;
-	char        *val;
-}               t_keyval;
+	char		*key;
+	char		*val;
+}				t_keyval;
 
 /*
 ** minishell core
 */
 
-int			msh_loop(t_mshinfo *mshinfo);
-int			msh_get_next_cmd(t_mshinfo *mshinfo, char **cmd);
-int			msh_exec_cmd(t_mshinfo *mshinfo, char *cmd);
+int				msh_loop(t_mshinfo *mshinfo);
+int				msh_get_next_cmd(t_mshinfo *mshinfo, char **cmd);
+int				msh_exec_cmd(t_mshinfo *mshinfo, char *cmd);
 
 /*
 ** minishell utils
 */
 
-t_list		*msh_parse_envp(char **envp);
-void		*msh_put_errmsg(t_mshinfo *mshinfo);
-void		msh_mshinfo_init(t_mshinfo *mshinfo);
-void		msh_mshinfo_free(t_mshinfo *mshinfo);
+t_list			*msh_parse_envp(char **envp);
+char			**mrt_split_cmd_to_argv(char *cmd, int *argc);
+void			*msh_put_errmsg(t_mshinfo *mshinfo);
+void			msh_mshinfo_init(t_mshinfo *mshinfo);
+void			msh_mshinfo_free(t_mshinfo *mshinfo);
 
 /*
 ** minishell 終了時用の関数
@@ -80,14 +81,62 @@ void		msh_mshinfo_free(t_mshinfo *mshinfo);
 ** 	msh_ext_by_err : mallocの失敗等、プログラム実行中のエラーにより終了する（エラ〜メッセージを出力する）
 */
 
-int			msh_exit_by_cmd(t_mshinfo *mshinfo);
-int			msh_exit_by_err(t_mshinfo *mshinfo);
+int				msh_exit_by_cmd(t_mshinfo *mshinfo);
+int				msh_exit_by_err(t_mshinfo *mshinfo);
+
+/*
+** echo
+*/
+
+int				msh_echo(int argc, char **argv, t_mshinfo *mshinfo);
+
+/*
+** cd
+*/
+
+int				msh_cd(int argc, char **argv, t_mshinfo *mshinfo);
+
+/*
+** pwd
+*/
+
+int				msh_pwd(int argc, char **argv, t_mshinfo *mshinfo);
+
+/*
+** export
+*/
+
+int				msh_export(int argc, char **argv, t_mshinfo *mshinfo);
+
+/*
+** unset
+*/
+
+int				msh_unset(int argc, char **argv, t_mshinfo *mshinfo);
+
+/*
+** env
+*/
+
+int				msh_env(int argc, char **argv, t_mshinfo *mshinfo);
+
+/*
+** exit
+*/
+
+int				msh_exit(int argc, char **argv, t_mshinfo *mshinfo);
+
+/*
+** exec_exceutable
+*/
+
+int				msh_executable(int argc, char **argv, t_mshinfo *mshinfo);
 
 /*
 ** t_keyval utils
 ** t_keyval用便利関数
 */
 
-void		msh_keyval_free(t_keyval *keyval);
+void			msh_keyval_free(void *keyval);
 
 #endif
