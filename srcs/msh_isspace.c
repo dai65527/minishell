36e0e38db4726acba7f70a67c2504dcc124f1d4d
dnakano <dnakano@student.42tokyo.jp>
2020-12-21 07:59:11 +0900

    Fix: function name of msh_is_space
    - mismatch of filename (msh_isspace.c) and functionname (msh_isspace)
    - modified function name to msh_isspace

--- srcs/msh_isspace.c
+++ srcs/msh_isspace.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   msh_isspace.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dhasegaw <dhasegaw@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 02:41:56 by dhasegaw          #+#    #+#             */
-/*   Updated: 2020/12/16 23:48:14 by dhasegaw         ###   ########.fr       */
+/*   Updated: 2020/12/21 07:56:41 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int			msh_is_space(char c)
+int			msh_isspace(char c)
 {
 	return (c == ' ' || c == '\t' || c == '\v' || c == '\f' || c == '\r');
 }
