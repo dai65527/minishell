51e4d7fea7f65ac2c425559a0552a92074fe8e4e
dnakano <dnakano@student.42tokyo.jp>
2020-12-21 10:44:03 +0900

    Fix: main.c
    - 終了時msh_loopの返り値を返すように変更

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 18:37:15 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/20 18:12:20 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/12/21 09:17:45 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@
 
 int		main(int argc, char **argv, char **envp)
 {
+	int			ret;
 	t_mshinfo	mshinfo;
 
 	msh_mshinfo_init(&mshinfo);
@@ -39,7 +40,7 @@ int		main(int argc, char **argv, char **envp)
 		mshinfo.fd_cmdsrc = FD_STDIN;
 	else if ((mshinfo.fd_cmdsrc = open(argv[1], O_RDONLY)) < 0)
 		return (msh_puterr("bash", 1));
-	msh_loop(&mshinfo);
+	ret = msh_loop(&mshinfo);
 	msh_mshinfo_free(&mshinfo);
-	return (0);
+	return (ret);
 }
