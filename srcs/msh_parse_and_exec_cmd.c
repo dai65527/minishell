8341547c1545e17e4b9dd75ad9500baa00d74701
dnakano <dnakano@student.42tokyo.jp>
2020-12-22 16:37:36 +0900

    Improve: add some comment
    - add some comment about return value of parse_and_exec_cmd

--- srcs/msh_parse_and_exec_cmd.c
+++ srcs/msh_parse_and_exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 10:05:38 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/21 21:13:55 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/12/22 16:36:52 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,12 +56,19 @@ static char		**arglst_to_argv(t_list **arglst)
 **	- Convert mshinfo->arglst to argv by sub function arglst_to_argv.
 **	- If msh_parse_to_arglst returns 0, command has not read enough so return 0
 **	  to read more.
-**	- Else if msh_parse_to_arglst returns 1 or 2, all command and arguments has
-**	  stored in mshinfo->arglst, so break loop and execute command.
+**	- Else if msh_parse_to_arglst returns 1 or 2, all command and arguments
+**	  has successfully stored in mshinfo->arglst, so break loop and continue to
+**	  execute command.
 **	- If if msh_parse_to_arglst returns 2, it means command has ended by ';' and
 **	  still have command to read in save, so fold flg_gonext.
 **	- Else, (this means 3 has returned), need to make process for pipe by
 **	  calling msh_create_pipe and continue to start of the loop.
+**	- Msh_exec_cmd will execute command. This function returns non 0 pid if new
+**	  child process has been forked inside the function. Otherwise it returns 0.
+**	- Function parse_and_exec finally returns the pid returned by msh_exec_cmd
+**	  function so that msh_wait can wait for it and properly store the status
+**	  code to mshinfo->ret_last_cmd. But incase pid = 0, return -1. (0 is
+**	  already used when need to continue to read.)
 */
 
 static pid_t	parse_and_exec(t_mshinfo *mshinfo, char **save, int *flg_gonext)
