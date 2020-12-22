01b7a5d79838c57b3cea5efbc8a0ad9fdbe2efe1
dnakano <dnakano@student.42tokyo.jp>
2020-12-22 15:30:42 +0900

    Fix: error msg when command not found.
    FROM: `minishell: command not found: xxx`
    TO: `minishell: xxx: command not found`

--- srcs/msh_find_and_copy_path.c
+++ srcs/msh_find_and_copy_path.c
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 18:21:28 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/21 12:39:43 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/12/22 15:24:12 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,8 +114,9 @@ int			msh_find_and_copy_path(char **argv, t_mshinfo *mshinfo, char *path)
 		{
 			if (!errno)
 			{
-				ft_putstr_fd("minishell: command not found: ", 2);
-				ft_putendl_fd(argv[0], 2);
+				ft_putstr_fd("minishell: ", FD_STDERR);
+				ft_putstr_fd(argv[0], FD_STDERR);
+				ft_putstr_fd(": command not found\n", FD_STDERR);
 			}
 			return (-1);
 		}
