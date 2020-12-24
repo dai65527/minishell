c271eee5c1ef1e6fbac1ab6393f30f4a2d6e4ef6
{TrainCoffeeBoy} <{oumeitaro@gmail.com}>
2020-12-25 02:20:57 +0900

    refactoring based on dnakno comments

--- srcs/msh_echo.c
+++ srcs/msh_echo.c
@@ -6,7 +6,7 @@
 /*   By: dhasegaw <dhasegaw@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 20:36:14 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/24 20:03:08 by dhasegaw         ###   ########.fr       */
+/*   Updated: 2020/12/25 02:18:11 by dhasegaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,15 @@ int				msh_echo(t_mshinfo *mshinfo, char **argv, int flg_forked)
 
 	if (!argv[1])
 		ft_putstr_fd("\n", FD_STDOUT);
-	else if (!ft_strncmp(argv[1], "-n", 3) && !argv[2])
-		;
 	else
 	{
 		flg_omit_newline = (!ft_strncmp(argv[1], "-n", 3)) ? 1 : 0;
-		i = 0;
-		while (argv[++i + flg_omit_newline])
+		i = flg_omit_newline;
+		while (argv[++i])
 		{
-			if (i != 1)
+			if (i != 1 + flg_omit_newline)
 				ft_putstr_fd(" ", FD_STDOUT);
-			ft_putstr_fd(argv[i + flg_omit_newline], FD_STDOUT);
+			ft_putstr_fd(argv[i], FD_STDOUT);
 		}
 		if (!flg_omit_newline)
 			ft_putstr_fd("\n", FD_STDOUT);
