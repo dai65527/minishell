6f9b50362959b708a90daff1875793afb76ff51e
{TrainCoffeeBoy} <{oumeitaro@gmail.com}>
2020-12-24 20:07:39 +0900

    if flag_forked exist, use exit in stead of return

--- srcs/msh_echo.c
+++ srcs/msh_echo.c
@@ -6,18 +6,18 @@
 /*   By: dhasegaw <dhasegaw@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 20:36:14 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/23 22:56:20 by dhasegaw         ###   ########.fr       */
+/*   Updated: 2020/12/24 20:03:08 by dhasegaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdlib.h>
 
 int				msh_echo(t_mshinfo *mshinfo, char **argv, int flg_forked)
 {
 	int	flg_omit_newline;
 	int	i;
 
-	(void)flg_forked;
 	if (!argv[1])
 		ft_putstr_fd("\n", FD_STDOUT);
 	else if (!ft_strncmp(argv[1], "-n", 3) && !argv[2])
@@ -36,5 +36,7 @@ int				msh_echo(t_mshinfo *mshinfo, char **argv, int flg_forked)
 			ft_putstr_fd("\n", FD_STDOUT);
 	}
 	mshinfo->ret_last_cmd = 0;
+	if (flg_forked)
+		exit(0);
 	return (0);
 }
