be0bb3c8f0d39a0d0bf4a72e9be75050e607b5dd
{TrainCoffeeBoy} <{oumeitaro@gmail.com}>
2020-12-25 00:28:32 +0900

    cd ok

--- srcs/msh_cd.c
+++ srcs/msh_cd.c
@@ -6,7 +6,7 @@
 /*   By: dhasegaw <dhasegaw@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 20:36:14 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/25 00:09:28 by dhasegaw         ###   ########.fr       */
+/*   Updated: 2020/12/25 00:26:30 by dhasegaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 #include <stdlib.h>
 
 #define CMD "cd"
-#define HOME "HOME"
 
-static char		*get_home_from_envlst(t_mshinfo *mshinfo)
+static char		*get_home_dir(t_mshinfo *mshinfo)
 {
 	t_list		*head;
 	t_keyval	*env;
@@ -26,7 +25,7 @@ static char		*get_home_from_envlst(t_mshinfo *mshinfo)
 	while (head)
 	{
 		env = head->content;
-		if (!ft_strncmp(env->key, HOME, ft_strlen(HOME) + 1))
+		if (!ft_strncmp(env->key, "HOME", ft_strlen("HOME") + 1))
 			return (env->val);
 		head = head->next;
 	}
@@ -35,17 +34,17 @@ static char		*get_home_from_envlst(t_mshinfo *mshinfo)
 
 int				msh_cd(t_mshinfo *mshinfo, char **argv, int flg_forked)
 {
-	ft_putendl_fd(get_home_from_envlst(mshinfo), 1);
-	char *key = ft_strdup(HOME);
 	if (mshinfo->has_pipe)
 		;
-	else if (!argv[1] && (chdir(msh_get_value_from_envlst(mshinfo, &key)) < 0))
-	// else if (!argv[1] && (chdir(get_home_from_envlst(mshinfo)) < 0))
+	else if (!argv[1])
 	{
-		mshinfo->ret_last_cmd = 1;
-		if (flg_forked)
-			exit(msh_puterr(MSH_NAME, CMD, -1));
-		return (msh_puterr(MSH_NAME, CMD, -1));
+		if ((chdir(get_home_dir(mshinfo))) < 0)
+		{
+			mshinfo->ret_last_cmd = 1;
+			if (flg_forked)
+				exit(msh_puterr(MSH_NAME, CMD, -1));
+			return (msh_puterr(MSH_NAME, CMD, -1));
+		}
 	}
 	else if ((chdir(argv[1])) < 0)
 	{
