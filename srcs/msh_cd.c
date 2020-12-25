d0b7f376c53c5fa89704cd5f1cc5ff131c75b2f2
{TrainCoffeeBoy} <{oumeitaro@gmail.com}>
2020-12-25 23:28:16 +0900

    change of PWD and OLDPWD is OK

--- srcs/msh_cd.c
+++ srcs/msh_cd.c
@@ -6,7 +6,7 @@
 /*   By: dhasegaw <dhasegaw@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 20:36:14 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/25 12:26:57 by dhasegaw         ###   ########.fr       */
+/*   Updated: 2020/12/25 23:27:20 by dhasegaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 #define CMD "cd"
 
-static char		*get_home_dir(t_mshinfo *mshinfo)
+static char		*get_env(t_mshinfo *mshinfo, char *key)
 {
 	t_list		*head;
 	t_keyval	*env;
@@ -27,7 +27,7 @@ static char		*get_home_dir(t_mshinfo *mshinfo)
 	while (head)
 	{
 		env = head->content;
-		if (!ft_strncmp(env->key, "HOME", ft_strlen("HOME") + 1))
+		if (!ft_strncmp(env->key, key, ft_strlen(key) + 1))
 			return (env->val);
 		head = head->next;
 	}
@@ -51,29 +51,55 @@ static int		cd_err(char *dirname, int ret)
 	return (ret);
 }
 
+static int		change_env(t_mshinfo *mshinfo)
+{
+	t_list		*envlst;
+	t_keyval	*env;
+	char		*old;
+	char		*present;
+
+	envlst = mshinfo->envlst;
+	if (!(old = ft_strdup(get_env(mshinfo, "PWD"))))
+		return (-1);
+	if (!(present = getcwd(NULL, 0)))
+		return (-1);
+	while (envlst)
+	{
+		env = envlst->content;
+		if (!ft_strncmp("OLDPWD", env->key, ft_strlen("OLDPWD") + 1))
+			msh_free_set(&(env->val), old);
+		else if (!ft_strncmp("PWD", env->key, ft_strlen("PWD") + 1))
+			msh_free_set(&(env->val), present);
+		envlst = envlst->next;
+	}
+	return (0);
+}
+
 int				msh_cd(t_mshinfo *mshinfo, char **argv, int flg_forked)
 {
+	int	ret;
+
+	ret = 0;
 	if (mshinfo->has_pipe)
 		;
 	else if (!argv[1])
 	{
-		if ((chdir(get_home_dir(mshinfo))) < 0)
-		{
-			mshinfo->ret_last_cmd = 1;
-			if (flg_forked)
-				exit(cd_err(NULL, -1));
-			return (cd_err(NULL, -1));
-		}
+		if ((chdir(get_env(mshinfo, "HOME"))) < 0)
+			ret = -1;
 	}
 	else if ((chdir(argv[1])) < 0)
+		ret = -1;
+	if (change_env(mshinfo) < 0)
+		ret = -1;
+	if (ret < 0)
 	{
-		mshinfo->ret_last_cmd = 1;
+		mshinfo->ret_last_cmd = (unsigned int)ret;
 		if (flg_forked)
-			exit(cd_err(argv[1], -1));
-		return (cd_err(argv[1], -1));
+			exit(cd_err(argv[1], ret));
+		return (cd_err(argv[1], ret));
 	}
-	mshinfo->ret_last_cmd = 0;
+	mshinfo->ret_last_cmd = ret;
 	if (flg_forked)
-		exit(0);
-	return (0);
+		exit(ret);
+	return (ret);
 }
