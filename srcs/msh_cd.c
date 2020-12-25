8a5f8fb964d2299c32bfd4cdfdc2dfabb73c45de
{TrainCoffeeBoy} <{oumeitaro@gmail.com}>
2020-12-25 12:27:24 +0900

    err msg is modified

--- srcs/msh_cd.c
+++ srcs/msh_cd.c
@@ -6,13 +6,15 @@
 /*   By: dhasegaw <dhasegaw@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 20:36:14 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/25 00:26:30 by dhasegaw         ###   ########.fr       */
+/*   Updated: 2020/12/25 12:26:57 by dhasegaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 #define CMD "cd"
 
@@ -32,6 +34,23 @@ static char		*get_home_dir(t_mshinfo *mshinfo)
 	return (NULL);
 }
 
+static int		cd_err(char *dirname, int ret)
+{
+	ft_putstr_fd(MSH_NAME, FD_STDERR);
+	ft_putstr_fd(": ", FD_STDERR);
+	ft_putstr_fd(CMD, FD_STDERR);
+	ft_putstr_fd(": ", FD_STDERR);
+	if (!dirname)
+		ft_putendl_fd("HOME not set", FD_STDERR);
+	else
+	{
+		ft_putstr_fd(dirname, FD_STDERR);
+		ft_putstr_fd(": ", FD_STDERR);
+		ft_putendl_fd(strerror(errno), FD_STDERR);
+	}
+	return (ret);
+}
+
 int				msh_cd(t_mshinfo *mshinfo, char **argv, int flg_forked)
 {
 	if (mshinfo->has_pipe)
@@ -42,16 +61,16 @@ int				msh_cd(t_mshinfo *mshinfo, char **argv, int flg_forked)
 		{
 			mshinfo->ret_last_cmd = 1;
 			if (flg_forked)
-				exit(msh_puterr(MSH_NAME, CMD, -1));
-			return (msh_puterr(MSH_NAME, CMD, -1));
+				exit(cd_err(NULL, -1));
+			return (cd_err(NULL, -1));
 		}
 	}
 	else if ((chdir(argv[1])) < 0)
 	{
 		mshinfo->ret_last_cmd = 1;
 		if (flg_forked)
-			exit(msh_puterr(MSH_NAME, CMD, -1));
-		return (msh_puterr(MSH_NAME, CMD, -1));
+			exit(cd_err(argv[1], -1));
+		return (cd_err(argv[1], -1));
 	}
 	mshinfo->ret_last_cmd = 0;
 	if (flg_forked)
