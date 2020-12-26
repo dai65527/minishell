aa8ae51ce5992fb9bd789f17f678351d2823dffb
{TrainCoffeeBoy} <{oumeitaro@gmail.com}>
2020-12-26 15:24:00 +0900

    invalid kye handling, issue36 is OK with norm

--- srcs/msh_get_value_from_envlst.c
+++ srcs/msh_get_value_from_envlst.c
@@ -6,11 +6,32 @@
 /*   By: dhasegaw <dhasegaw@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 02:49:51 by dhasegaw          #+#    #+#             */
-/*   Updated: 2020/12/26 14:51:20 by dhasegaw         ###   ########.fr       */
+/*   Updated: 2020/12/26 15:22:25 by dhasegaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdlib.h>
+
+static int		get_invalid_key(char **key, char **invalid_key)
+{
+	if (!(ft_isalpha(*key[0]) || *key[0] == '_'))
+	{
+		if (!(*invalid_key = ft_strdup("$")))
+			return (-1);
+		msh_free_set(invalid_key, ft_strjoin(*invalid_key, *key));
+		msh_free_setnull((void**)key);
+		return (1);
+	}
+	return (0);
+}
+
+static char		*return_val_free_key(char *ret, char ***key)
+{
+	free(**key);
+	*key = NULL;
+	return (ret);
+}
 
 char			*msh_get_value_from_envlst(t_mshinfo *mshinfo, char **key,
 											int flg_quote)
@@ -20,27 +41,15 @@ char			*msh_get_value_from_envlst(t_mshinfo *mshinfo, char **key,
 	char		*invalid_key;
 
 	if (!*key)
-	{
-		msh_free_setnull((void**)key);
-		return (NULL);
-	}
-	if (!(ft_isalpha(*key[0]) || *key[0] == '_'))
-	{
-		if (!(invalid_key = ft_strdup("$")))
-			return (NULL);
-		msh_free_set(&invalid_key, ft_strjoin(invalid_key, *key));
-		msh_free_setnull((void**)key);
+		return (return_val_free_key(NULL, &key));
+	if (get_invalid_key(key, &invalid_key))
 		return (invalid_key);
-	}
 	head = mshinfo->envlst;
 	while (head)
 	{
 		env = head->content;
 		if (!ft_strncmp(env->key, *key, ft_strlen(*key) + 1))
-		{
-			msh_free_setnull((void**)key);
-			return (env->val);
-		}
+			return (return_val_free_key(NULL, &key));
 		head = head->next;
 	}
 	msh_free_setnull((void**)key);
