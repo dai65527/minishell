033d928c183fa18dbb9eb63f65ff5fedc9e1cef7
{TrainCoffeeBoy} <{oumeitaro@gmail.com}>
2020-12-26 17:28:58 +0900

    handling special var is OK with norm but not for inside quote

--- srcs/msh_get_argv.c
+++ srcs/msh_get_argv.c
@@ -6,21 +6,49 @@
 /*   By: dhasegaw <dhasegaw@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 23:45:09 by dhasegaw          #+#    #+#             */
-/*   Updated: 2020/12/26 16:15:26 by dhasegaw         ###   ########.fr       */
+/*   Updated: 2020/12/26 17:27:12 by dhasegaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int		store_content(char ***content, char *val)
+static int		store_content(char ***content, char **val)
 {
 	if (!**content)
-		**content = ft_strdup(val);
+		**content = ft_strdup(*val);
 	else
-		msh_free_set(*content, ft_strjoin(**content, val));
+		msh_free_set(*content, ft_strjoin(**content, *val));
+	msh_free_setnull((void**)val);
 	if (!**content)
 		return (-1);
 	return (0);
+}
+
+static ssize_t	handle_special_var(t_mshinfo *mshinfo, char *save,
+									char ***content, ssize_t *len)
+{
+	char	*val;
+	ssize_t	begin;
+
+	if (save[*len] != '?')
+		return (0);
+	val = ft_itoa(mshinfo->ret_last_cmd);
+	if (!val)
+		return (-1);
+	if (store_content(content, &val))
+		return (-1);
+	begin = ++(*len);
+	while (msh_check_operator(save, *len, "$><| \t\'\"\n;"))
+		(*len)++;
+	if (begin == *len && save[*len] != '$')
+		return (1);
+	if (begin != *len)
+	{
+		msh_free_set(&val, ft_substr(save, begin, *len - begin));
+		if (store_content(content, &val))
+			return (-1);
+	}
+	return (1);
 }
 
 static ssize_t	msh_get_env(t_mshinfo *mshinfo, char *save,
@@ -36,13 +64,17 @@ static ssize_t	msh_get_env(t_mshinfo *mshinfo, char *save,
 	while (msh_check_operator(save, len, "><| \t\'\"\n;"))
 	{
 		begin = ++len;
+		if ((ret = handle_special_var(mshinfo, save, &content, &len)) < 0)
+			return (-1);
+		if (ret)
+			continue ;
 		while (msh_check_operator(save, len, "$><| \t\'\"\n;"))
 			len++;
 		if (!(key = ft_substr(save, begin, len - begin)))
 			return (-1);
 		if (!(val = msh_get_value_from_envlst(mshinfo, &key, 0)))
 			return (len - (begin - 1));
-		if (store_content(&content, val))
+		if (store_content(&content, &val))
 			return (-1);
 	}
 	return (len - (begin - 1));
