259dd944948bf79864a121a1962a932815310e69
dnakano <dnakano@student.42tokyo.jp>
2020-12-28 12:38:53 +0900

    Fix: msh_wait (exit_code)
    exitコード修正しました。修正箇所はmsh_waitのみです。
    - signalで終了された時->signalコード + 128 (man bash)
    - signal以外で終了された時。WEXITSTATUSで終了コードを取得。
    - signalで終了されたかどうかはマクロWIFSIGALEDで確認します。

--- srcs/msh_wait.c
+++ srcs/msh_wait.c
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 21:01:44 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/20 21:30:24 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/12/28 12:35:14 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ void		msh_wait(t_mshinfo *mshinfo, pid_t pid)
 	while (mshinfo->n_proc > 0)
 	{
 		if (wait(&status) == pid)
-			mshinfo->ret_last_cmd = status;
+			mshinfo->ret_last_cmd = WIFSIGNALED(status) ?
+										status + 128 : WEXITSTATUS(status);
 		mshinfo->n_proc--;
 	}
 }
