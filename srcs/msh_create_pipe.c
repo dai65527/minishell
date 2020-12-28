894f5917de8a0f143707760d922dd70951f69951
dnakano <dnakano@student.42tokyo.jp>
2020-12-28 13:06:05 +0900

    Fix: msh_create_pipe
    - fork失敗時のエラー出力追加
    - fork失敗時にflg_errinparseを立てるように変更

--- srcs/msh_create_pipe.c
+++ srcs/msh_create_pipe.c
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 10:49:56 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/24 08:35:17 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/12/28 13:04:37 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ pid_t		msh_create_pipe(t_mshinfo *mshinfo, char **argv)
 	{
 		close(pipe_fd[0]);
 		close(pipe_fd[1]);
-		return (-1);
+		mshinfo->flg_errinparse = 1;
+		return (msh_puterr(MSH_NAME, "fork", -1));
 	}
 	else if (pid == 0)
 	{
